840c663f85cb9c70dcf13d8ed290db658e82a21c
Dev <USER@HP>
2022-03-23 17:24:52 +0530

    Second Commit

---
+++ com.crm.vtiger.Sdet31/src/test/java/com/crm/Vtiger/Demo/NewAddedClassTest.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger.Demo;
+
+import org.testng.annotations.Test;
+
+public class NewAddedClassTest {
+	@Test
+	public void newadd()
+	{
+		System.out.println("New Added Program in GitHub");
+	}
+
+}
