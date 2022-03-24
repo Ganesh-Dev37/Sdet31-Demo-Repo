a111bbf93b0e33eb486653ea9c4fd00bdf1e2f74
USER <USER@HP>
2022-03-24 14:13:18 +0530

    User1 Commit

---
+++ com.crm.vtiger.Sdet31/src/test/java/com/crm/Vtiger/Demo/NewGitHub.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger.Demo;
+
+import org.testng.annotations.Test;
+
+public class NewGitHub {
+	@Test
+	public void git()
+	{
+		System.out.println("User1 script created");
+	}
+
+}
